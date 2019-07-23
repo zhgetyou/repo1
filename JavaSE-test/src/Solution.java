9c272ef349c1972404f74d55a7da1134606016ea
zhgetyou <239494676@qq.com>
2019-07-23 09:42:57 +0800

    第一次使用git提交Java项目

---
+++ JavaSE-test/src/Solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    // Parameters:
+    //    numbers:     an array of integers
+    //    length:      the length of array numbers
+    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    public  boolean duplicate(int numbers[],int length,int [] duplication) {
+        if(numbers==null||length<=0)
+            return false;
+        for(int i=0;i<length;i++){
+            while(numbers[i]!=i){
+                if(numbers[i]==numbers[numbers[i]]){
+                    duplication[0]=numbers[i];
+                    return true;
+                }
+                swap(numbers,i,numbers[i]);
+            }
+        }
+        return false;
+
+
+    }
+    private void swap(int [] numbers,int i,int j){
+        int t =numbers[i];
+        numbers[i]=numbers[j];
+        numbers[j]=t;
+    }
+    public  void main (String [] args) {
+        int [] numbers={2,3,1,0,2,5,3};
+        int [] a = new int[7];
+
+        duplicate(numbers,7,a);
+    }
+}
